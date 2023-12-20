1ef5013367295856b985a1e191c175b1aebfc092
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2023-12-20 12:39:23 +0100

    Add controller, routes and login page with some styling

--- public/index.php
+++ public/index.php
@@ -2,12 +2,27 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
 
+/*===> ACCOUNTS <===*/
 
+/* LOGIN */
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
+/* ACCOUNT RECOVERY */
+$router->get('/forgot-password', [LoginController::class, 'forgotPassword']);
+$router->post('/forgot-password', [LoginController::class, 'forgotPassword']);
+$router->get('/recover', [LoginController::class, 'recover']);
+$router->post('/recover', [LoginController::class, 'recover']);
+
+/* REGISTER */
+$router->get('/create-account', [LoginController::class, 'register']);
+$router->post('/create-account', [LoginController::class, 'register']);
 
 // Checks if the routes are valid and assings functions in the controller
 $router->checkRoutes();
